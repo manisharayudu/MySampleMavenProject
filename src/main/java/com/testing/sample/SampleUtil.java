a100ed3775b7c2b3c09082e44821762d42a585a3
Manisha Rayudu <83665305+manisharayudu@users.noreply.github.com>
2022-02-23 10:16:37 +0100

    Update SampleUtil.java

--- src/main/java/com/testing/sample/SampleUtil.java
+++ src/main/java/com/testing/sample/SampleUtil.java
@@ -1,5 +1,13 @@
 package com.testing.sample;
 
 public class SampleUtil {
-
+public static Integer add(Integer a, Integer b) {
+        if (a == null) {
+            return null;
+        }
+        if (b == null) {
+            return null;
+        }
+        return a + b;
+    }
 }
